c4fb7ea2021991d0e25a17ee3511b8f57d33ead9
aslamm <aslamm.anas29@gmail.com>
2020-02-17 20:57:29 -0500

    Updates

---
+++ precache-manifest.cbeaff6df11fa7c682c9834822ba88b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d363e06e3a6d4e980ef1ab80624362d1",
+    "url": "/Prime-Steak/index.html"
+  },
+  {
+    "revision": "5ec43b2b3da458f97f8d",
+    "url": "/Prime-Steak/static/css/main.c1092c42.chunk.css"
+  },
+  {
+    "revision": "d6d34265f97e5e34005a",
+    "url": "/Prime-Steak/static/js/2.e402502d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Prime-Steak/static/js/2.e402502d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ec43b2b3da458f97f8d",
+    "url": "/Prime-Steak/static/js/main.1bc336fe.chunk.js"
+  },
+  {
+    "revision": "e14119652720d08080cd",
+    "url": "/Prime-Steak/static/js/runtime-main.61125326.js"
+  }
+]);
